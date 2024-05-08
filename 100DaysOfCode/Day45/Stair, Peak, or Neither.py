c8fa40c0f53a7b19a3633a774121072f7ef55253
Vyangesh <joshisonu252@gmail.com>
2024-05-08 09:24:32 +0530

    ADD:Day45

---
+++ 100DaysOfCode/Day45/Stair, Peak, or Neither.py
@@ -0,0 +1,10 @@
+#problem statement: https://codeforces.com/problemset/problem/1950/A
+
+for _ in range(int(input())):
+    a,b,c=map(int,input().split())
+    if a<b and b<c:
+        print("STAIR")
+    elif a<b and b>c:
+        print("PEAK")
+    else:
+        print("NONE")

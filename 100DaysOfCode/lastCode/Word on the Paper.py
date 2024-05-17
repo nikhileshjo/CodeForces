d2b69808607d7c6e5af7668a489f93252282198e
Vyangesh <joshisonu252@gmail.com>
2024-05-17 09:11:41 +0530

    ADD:lastCode

---
+++ 100DaysOfCode/lastCode/Word on the Paper.py
@@ -0,0 +1,7 @@
+#problem statment: https://codeforces.com/problemset/problem/1850/C
+
+for _ in range(int(input())):
+    rows=''
+    for i in range(8):
+        rows+=input().strip(".")
+    print(rows)
